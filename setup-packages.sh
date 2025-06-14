6919bd1bdc575269e0c4bc163678b6aab884ce1e
Valentin Mücklausch <valentin.muecklausch@outlook.de>
2025-06-14 23:32:16 +0200

    debs...

--- setup-packages.sh
+++ setup-packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo pacman -S --needed waybar hyprland wofi kitty fzf zoxide ttf-jetbrains-mono-nerd hyprpaper helix firefox zsh timeshift wl-clipboard openssh sddm unzip bwrap
+sudo pacman -S --needed waybar hyprland wofi kitty fzf zoxide ttf-jetbrains-mono-nerd hyprpaper helix firefox zsh timeshift wl-clipboard openssh sddm unzip bubblewrap
 
 sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && cd .. && rm -rf yay
 

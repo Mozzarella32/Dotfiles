44a8bd1e6411e60100bbd7d7f5cf7dc1a134e34e
Valentin Mücklausch <valentin.muecklausch@outlook.de>
2025-06-14 23:33:49 +0200

    yay

--- setup-packages.sh
+++ setup-packages.sh
@@ -2,6 +2,13 @@
 
 sudo pacman -S --needed waybar hyprland wofi kitty fzf zoxide ttf-jetbrains-mono-nerd hyprpaper helix firefox zsh timeshift wl-clipboard openssh sddm unzip bubblewrap
 
-sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && cd .. && rm -rf yay
+if ! command -v yay >/dev/null 2>&1; then
+    sudo pacman -S --needed git base-devel
+    git clone https://aur.archlinux.org/yay.git
+    cd yay
+    makepkg -si --noconfirm
+    cd ..
+    rm -rf yay
+fi
 
 yay -S --needed intellij-idea-ultimate-edition

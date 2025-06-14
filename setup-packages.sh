61af75253c55f2d57eee37692cf601113e17da96
Valentin Mücklausch <valentin.muecklausch@outlook.de>
2025-06-14 21:26:03 +0200

    yay fix

--- setup-packages.sh
+++ setup-packages.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-sudo pacman -S --needed hyprland wofi kitty fzf zoxide ttf-jetbrains-mono-nerd hyprpaper helix firefox zsh timeshift wl-clipboard openssh yay sddm
+sudo pacman -S --needed hyprland wofi kitty fzf zoxide ttf-jetbrains-mono-nerd hyprpaper helix firefox zsh timeshift wl-clipboard openssh sddm
+
+sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 
 yay -S --needed intellij-idea-ultimate-edition
 

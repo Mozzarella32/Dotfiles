cd2b3bddd6e006d6fbac3d0c9ec2b7486e619504
Valentin Mücklausch <valentin.muecklausch@outlook.de>
2025-06-15 20:33:42 +0200

    packages

--- setup-packages.sh
+++ setup-packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo pacman -S --needed --noconfirm waybar hyprland wofi kitty fzf zoxide ttf-jetbrains-mono-nerd hyprpaper helix firefox zsh timeshift wl-clipboard openssh sddm unzip bubblewrap brightnessctl tk python3
+sudo pacman -S --needed --noconfirm waybar hyprland wofi kitty fzf zoxide ttf-jetbrains-mono-nerd hyprpaper helix firefox zsh timeshift wl-clipboard openssh sddm unzip bubblewrap brightnessctl python
 
 if ! command -v yay >/dev/null 2>&1; then
     sudo pacman -S --needed --noconfirm git base-devel

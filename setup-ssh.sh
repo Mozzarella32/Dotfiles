09f5686d611fa3a0bf6247ab86889604c77008d6
Valentin Mücklausch <valentin.muecklausch@outlook.de>
2026-04-15 23:21:06 +0200

    Make ssh a little bit less cursed

--- setup-ssh.sh
+++ setup-ssh.sh
@@ -7,13 +7,3 @@ if [ -d "$EXTERN_SSH" ]; then
     sudo cp -r "$EXTERN_SSH" "$HOME/.ssh"
     sudo chown -R $(whoami):$(whoami) "$HOME/.ssh"
 fi
-
-KEY="$HOME/.ssh/id_ed25519.pub"
-
-if [ -f "$KEY" ]; then
-    echo "SSH-Key $KEY exists."
-    exit 0
-else
-    echo "Generating SSH-Key"
-    ssh-keygen -t ed25519
-fi
